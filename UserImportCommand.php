59032741822eb2bc6428bc25fbe22a95cc25a30d
JamesS92 <JamesS92@users.noreply.github.com>
2014-11-07 16:06:51 +0000

    Create UserImportCommand.php

---
+++ UserImportCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+class UserImportCommand extends CConsoleCommand
+{
+	public $seperator = "\n"; 
+	public $dataDir = "/cubric/users/sapwe/Sites/CRS/protected/commands/shell/data"; 
+	public $fileName = "users.txt";
+	public $jobNo = 0;
+    public function run($args)
+    {
+    	    
+    	    $file = sprintf('%s/%s', $this->dataDir , $this->fileName); 
+	    $data = file_get_contents( $file );
+	    $job = array();
+	    
+	    if ($data)
+		    $users= explode($this->seperator, $data);
+	    
+	    foreach($users as $u)
+	    {
+	    	    $line = explode('  ', $u);
+	    	    $username = $line[0]; 
+	    	    $firstname = $line[1];
+	    	    if (isset($line[2]))
+	    	    	$surname = $line[2];
+	    	     
+	    	    
+	    	    	$user = User::model()->find('username=:username', array(':username'=>$username)); 
+	    	    	if ($user === null)
+	    	    	{
+	    	    		$user = new User; 
+	    	    		$user->username = $username; 
+	    	    		$user->firstname = $firstname;
+	    	    		$user->lastname = $surname; 
+	    	    		$user->save(); 
+	    	    		if ($user->hasErrors())
+	    	    			print_r($user->getErrors()); 
+	    	    			 
+	    	    	}
+	    } 
+    }
+}
